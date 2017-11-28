324cc6e0502ccb71f72ee7a22809e0fb0006c170
Sergio Guillen <serguimant@gmail.com>
2017-11-27 23:55:48 -0400

    Consume github api from cli entry point

--- golondrina.go
+++ golondrina.go
@@ -15,23 +15,19 @@ func foo() {
 		AppName:     "Golondrina",
 	})
 
-	token := os.Getenv("AUTH_TOKEN")
-	githubHost := os.Getenv("GITHUB_HOST")
-	if githubHost == "" {
-		githubHost = "github.com"
-	}
-
 	time.Sleep(1 * time.Second)
 	notify.Push("New Pull Request", "A new pull request for ReconMVS/recon-app", "", notificator.UR_NORMAL)
+
+}
+
+func run(host, token string, interval int) error {
 	p := github.Parser{
-		Host:  "ghe.coxautoinc.com",
+		Host:  host,
 		Token: token,
 	}
 	notifications, _ := p.GetNotifications()
 	fmt.Println(notifications)
-}
 
-func run(host, token string, interval int) error {
 	return nil
 }
 
